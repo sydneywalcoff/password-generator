bbba8257bc95e7ceae991aec72d728b7eb3c14be
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-17 12:29:32 -0800

    refactoring and adding validation that password fits requirements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 var generatePassword = function() {
   var accepted = [];
   var password = [];
-  
+  var matchFound;
+
   // password length
   var getPasswordLength = function() {
     var length = prompt("How many characters do you want your password to contain?");
@@ -12,54 +13,134 @@ var generatePassword = function() {
     }
     return length;
   }
-
   length = getPasswordLength();
 
-  var criteria = function() {
-    // lowercase
-    var lowercase = confirm("Would you like to include lowercase letters?");
-    var lowercaseChars = 'abcedfghijklmnopqrstuvwxyz';
-    lowercaseChars = lowercaseChars.split('');
+  // lowercase
+  var lowercase = confirm("Would you like to include lowercase letters?");
+  var lowercaseChars = 'abcedfghijklmnopqrstuvwxyz';
+  lowercaseChars = lowercaseChars.split('');
+  if(lowercase) {
+    accepted.push.apply(accepted, lowercaseChars);
+  }
+
+  // uppercase
+  var uppercase = confirm("Would you like to include uppercase letters?");
+  var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  uppercaseChars = uppercaseChars.split('');
+  if(uppercase) {
+    accepted.push.apply(accepted, uppercaseChars);
+  }
+
+  // numeric
+  var numbers = confirm("Would you like to include numbers?");
+  var numberChars = '0123456789';
+  numberChars = numberChars.split('');
+  if(numbers) {
+    accepted.push.apply(accepted, numberChars);
+  }
+
+  // special characters
+  var special = confirm("Would you like to include special characters?");
+  var specialChars = '!@#$%^&*()+-';
+  specialChars = specialChars.split('');
+  if(special) {
+    accepted.push.apply(accepted, specialChars);
+  }
+
+  if(!special && !numbers && !uppercase && !lowercase) {
+    alert("Please select at least one type of value for your password.");
+    return criteria();
+  }
+
+  
+  var createPassword = function() {
+    password = [];
+    for(let i=0; i < length; i++) {
+      randomNum = Math.floor(Math.random() * accepted.length);
+      randomDigit = accepted[randomNum];
+      password.push(randomDigit);
+    }
+  }
+
+  createPassword();
+
+  var inputvalidation = function() {
     if(lowercase) {
-      accepted.push.apply(accepted, lowercaseChars);
+      // nested for loop to cycle through each item in the arrays to look for matches
+      for (let i=0; i < length; i++) {
+        matchFound = false;
+
+        for (let j=0; j < lowercaseChars.length; j++) {
+          if(password[i] == lowercaseChars[j]) {
+            matchFound = true;
+            break;
+          }
+        }
+        if(!matchFound) {
+          createPassword();
+        }
+      }
     }
 
-    // uppercase
-    var uppercase = confirm("Would you like to include uppercase letters?");
-    var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    uppercaseChars = uppercaseChars.split('');
     if(uppercase) {
-      accepted.push.apply(accepted, uppercaseChars);
+      // nested for loop to cycle through each item in the arrays to look for matches
+      for (let i=0; i < length; i++) {
+        matchFound = false;
+
+        for (let j=0; j < uppercaseChars.length; j++) {
+          if(password[i] == uppercaseChars[j]) {
+            matchFound = true;
+            break;
+          }
+        }
+        return matchFound;
+        
+      }
+      if(!matchFound) {
+        createPassword();
+      }
     }
 
-    // numeric
-    var numbers = confirm("Would you like to include numbers?");
-    var numberChars = '0123456789';
-    numberChars = numberChars.split('');
     if(numbers) {
-      accepted.push.apply(accepted, numberChars);
+      // nested for loop to cycle through each item in the arrays to look for matches
+      for (let i=0; i < length; i++) {
+        matchFound = false;
+
+        for (let j=0; j < numberChars.length; j++) {
+          if(password[i] == numberChars[j]) {
+            matchFound = true;
+            break;
+          }
+        }
+        return matchFound;
+      }
+      if(!matchFound) {
+        createPassword();
+      }
     }
 
-    // special characters
-    var special = confirm("Would you like to include special characters?");
-    var specialChars = '!@#$%^&*()+-';
-    specialChars = specialChars.split('');
     if(special) {
-      accepted.push.apply(accepted, specialChars);
-    }
+      // nested for loop to cycle through each item in the arrays to look for matches
+      for (let i=0; i < length; i++) {
+        matchFound = false;
 
-    if(!special && !numbers && !uppercase && !lowercase) {
-      alert("Please select at least one type of value for your password.");
-      return criteria();
+        for (let j=0; j < specialChars.length; j++) {
+          if(password[i] == specialChars[j]) {
+            matchFound = true;
+            break;
+          }
+        }
+        return matchFound;
+      }
+
+      if(!matchFound) {
+        createPassword();
+      }
+      
+      
     }
   }
-  criteria();
-
-  for(let i=0; i < length; i++) {
-    randomNum = Math.floor(Math.random() * accepted.length);
-    randomDigit = accepted[randomNum];
-    password.push(randomDigit);
-  }
+  inputvalidation();
   password = password.join("");
   return password;
 }
