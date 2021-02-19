9482a1a6b055f7d9c329b47f47116dde8a6b056f
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-19 12:12:16 -0800

    all desired password character values appear in the final password

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,54 +12,93 @@ var generatePassword = function() {
     }
     return length;
   }
-
   length = getPasswordLength();
 
-  var criteria = function() {
+  var lowercase = confirm("Would you like to include lowercase letters?");
+  var lowercaseChars = 'abcedfghijklmnopqrstuvwxyz';
+  var uppercase = confirm("Would you like to include uppercase letters?");
+  var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var number = confirm("Would you like to include numbers?");
+  var numberChars = '0123456789';
+  var special = confirm("Would you like to include special characters?");
+  var specialChars = '!@#$%^&*()+-';
+
+  var getDesiredCharacters = function() {
     // lowercase
-    var lowercase = confirm("Would you like to include lowercase letters?");
-    var lowercaseChars = 'abcedfghijklmnopqrstuvwxyz';
     lowercaseChars = lowercaseChars.split('');
     if(lowercase) {
       accepted.push.apply(accepted, lowercaseChars);
     }
 
     // uppercase
-    var uppercase = confirm("Would you like to include uppercase letters?");
-    var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     uppercaseChars = uppercaseChars.split('');
     if(uppercase) {
       accepted.push.apply(accepted, uppercaseChars);
     }
 
     // numeric
-    var numbers = confirm("Would you like to include numbers?");
-    var numberChars = '0123456789';
+    
     numberChars = numberChars.split('');
-    if(numbers) {
+    if(number) {
       accepted.push.apply(accepted, numberChars);
     }
 
     // special characters
-    var special = confirm("Would you like to include special characters?");
-    var specialChars = '!@#$%^&*()+-';
     specialChars = specialChars.split('');
     if(special) {
       accepted.push.apply(accepted, specialChars);
     }
 
-    if(!special && !numbers && !uppercase && !lowercase) {
+    if(!special && !number && !uppercase && !lowercase) {
       alert("Please select at least one type of value for your password.");
       return criteria();
     }
-  }
-  criteria();
 
-  for(let i=0; i < length; i++) {
-    randomNum = Math.floor(Math.random() * accepted.length);
-    randomDigit = accepted[randomNum];
-    password.push(randomDigit);
-  }
+    return lowercase && uppercase && number && special;
+  };
+
+  console.log(getDesiredCharacters());
+
+  var validation = function(charType, charList) {
+    if(charType) {
+      matchFound= false;
+      for(let i = 0; i < length; i++) {
+
+        for (let j=0; j < charList.length; j++) {
+          if(password[i] === charList[j]) {
+            matchFound = true;
+            return matchFound;
+          }
+        }
+      }
+      if(!matchFound) {
+        createPassword();
+      }
+    }
+  };
+
+  var inputValidation = function() {
+    console.log(validation(lowercase, lowercaseChars));
+    console.log(validation(uppercase, uppercaseChars));
+    console.log(validation(number, numberChars));
+    console.log(validation(special, specialChars));
+  };
+
+  
+  var createPassword = function() {
+    password = [];
+    for(let i=0; i < length; i++) {
+      randomNum = Math.floor(Math.random() * accepted.length);
+      randomDigit = accepted[randomNum];
+      password.push(randomDigit);
+    }
+    inputValidation();
+    console.log(`password: ${password}`);
+  };
+
+  createPassword();
+  
+  
   password = password.join("");
   return password;
 }
